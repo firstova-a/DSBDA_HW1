6467eefb9e41907ca5a1289ebf22cb886ea72fd4
Firstova Anastasia <firstova-a@yandex.ru>
2021-03-21 22:53:51 +0300

    Make script idempotent

--- generate.sh
+++ generate.sh
@@ -4,6 +4,9 @@ X_MAX=20
 Y_MAX=20
 AREAS_NUMBER=4
 
-mkdir generated-data/ || true
+if [ -d "generated-data/" ]
+    mkdir generated-data/
+fi
+
 python3 generation-scripts/generate_areas.py $X_MAX $Y_MAX $AREAS_NUMBER
 python3 generation-scripts/generate_clicks.py $X_MAX $Y_MAX
